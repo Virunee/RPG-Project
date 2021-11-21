4b8caa16c7f635e094b365aabb0e1833dc9ee800
Virunee <viravongv@gmail.com>
2021-11-21 14:22:20 +0000

    Add namespaces

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Mover mover = GetComponent<Mover>();
+                mover.MoveToCursor();
+            }
+        }
+    }
+}
